6a1d1115d651fd67444571960858d00ad853a721
Taisei <taisei.960420@gmail.com>
2024-11-15 09:04:32 +0900

    メソッド

--- fortune_class.py
+++ fortune_class.py
@@ -8,35 +8,38 @@ FORTUNE_OUTPUT_TEMPLATE = """
 ラッキーナンバー: {lucky_number}
 """
 
-LUCKY_COLORS = ["red", "green", "blue"]
-LUCKY_NUMBERS = [1, 2, 3]
-
 class UserProfile:
   def __init__(self, name, birthday):
     self.name = name
     self.birthday = birthday
 
+class RandomFortuneTeller:
+  def __init__(self, lucky_colors, lucky_numbers):
+    self.lucky_colors = lucky_colors
+    self.lucky_numbers = lucky_numbers
 
-def tell_fortune(user_profile, today):
-  lucky_color = random.choice(LUCKY_COLORS)
-  lucky_number = random.choice(LUCKY_NUMBERS)
+  def tell(self, user_profile, today):
+    lucky_color = random.choice(self.lucky_colors)
+    lucky_number = random.choice(self.lucky_numbers)
 
-  return FORTUNE_OUTPUT_TEMPLATE.format(
-    today=today,
-    name=user_profile.name,
-    lucky_color=lucky_color,
-    lucky_number=lucky_number
-  )
+    return FORTUNE_OUTPUT_TEMPLATE.format(
+      today=today,
+      name=user_profile.name,
+      lucky_color=lucky_color,
+      lucky_number=lucky_number
+    )
 
 def main():
   name = "Alice"
   birthday = date(2000, 1, 1)
   user_profile = UserProfile(name, birthday)
-  print(user_profile.name)
-  print(user_profile.birthday)
   today = date.today()
 
-  result = tell_fortune(user_profile, today)
+  lucky_colors = ["red", "green", "blue"]
+  lucky_numbers = [1, 2, 3]
+  fortune_teller = RandomFortuneTeller(lucky_colors, lucky_numbers)
+
+  result = fortune_teller.tell(user_profile, today)
   print(result)
 
 if __name__ == "__main__":
